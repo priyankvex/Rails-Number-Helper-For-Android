295b1c6f8ccf3bf14c0f84679994d2bda5c2b460
Priyank Verma <priyankvex95@gmail.com>
2015-10-01 01:00:00 +0530

    Add NumberToPercentageConverter and NumberToPercentageConverter class.

---
+++ numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/NumberToPercentageConverter.java
@@ -0,0 +1,9 @@
+package com.wordpress.priyankvex.numberhelper;
+
+/**
+ * Created by Priyank(@priyankvex) on 1/10/15.
+ */
+public class NumberToPercentageConverter extends NumberConverter{
+
+
+}
---
+++ numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/builders/NumberToPercentageConverterBuilder.java
@@ -0,0 +1,8 @@
+package com.wordpress.priyankvex.numberhelper.builders;
+
+/**
+ * Created by Priyank(@priyankvex) on 1/10/15.
+ * Builder for {}
+ */
+public class NumberToPercentageConverterBuilder {
+}
