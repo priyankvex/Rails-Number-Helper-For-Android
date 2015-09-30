d003616167f88c0a1df315b77fbfe58415de8208
Priyank Verma <priyankvex95@gmail.com>
2015-10-01 01:33:20 +0530

    Add stub NumberToPhoneConverter.

--- numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/NumberToPhoneConverter.java
+++ numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/NumberToPhoneConverter.java
@@ -1,9 +1,37 @@
 package com.wordpress.priyankvex.numberhelper;
 
+import android.util.Log;
+
+import com.wordpress.priyankvex.numberhelper.exceptions.InvalidDelimiterException;
+import com.wordpress.priyankvex.numberhelper.exceptions.InvalidPrecisionException;
+import com.wordpress.priyankvex.numberhelper.exceptions.InvalidSeparatorException;
+import com.wordpress.priyankvex.numberhelper.exceptions.InvalidUnitException;
+
+import java.util.HashMap;
+
 /**
  * Created by Priyank(@priyankvex) on 1/10/15.
  */
 public class NumberToPhoneConverter extends NumberConverter{
 
+    public NumberToPhoneConverter(double rawNumber, HashMap<String, String> options){
+        this.options = options;
+        this.rawNumber = rawNumber;
 
+        // DEVELOPMENT
+        Log.d(Config.TAG, options.toString());
+        Log.d(Config.TAG, rawNumber + "");
+    }
+
+    public String convert() {
+        // Get the options
+        String countryCode = options.get(NumberConverter.KEY_COUNTRY_CODE);
+        // Validate options
+        // Options are valid. Start the converting process.
+        return resultNumber;
+    }
+
+    private void setPhoneNumber(){
+
+    }
 }
--- numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/builders/NumberToPhoneConverterBuilder.java
+++ numberhelper/src/main/java/com/wordpress/priyankvex/numberhelper/builders/NumberToPhoneConverterBuilder.java
@@ -1,7 +1,38 @@
 package com.wordpress.priyankvex.numberhelper.builders;
 
+import com.wordpress.priyankvex.numberhelper.NumberConverter;
+import com.wordpress.priyankvex.numberhelper.NumberToCurrencyConverter;
+import com.wordpress.priyankvex.numberhelper.NumberToPercentageConverter;
+
+import java.util.HashMap;
+
 /**
  * Created by Priyank(@priyankvex) on 1/10/15.
  */
 public class NumberToPhoneConverterBuilder {
+
+    private HashMap<String, String> options;
+    private double rawNumber;
+
+    public NumberToPhoneConverterBuilder(double rawNumber){
+        // Using a copy of default options.
+        this.options = NumberConverter.numberToCurrencyOptions;
+        this.rawNumber = rawNumber;
+    }
+
+    /**
+     * Sets the country code for the phone number
+     * @param countryCode country code as a String.
+     */
+    public void setCountryCode(String countryCode){
+        options.put(NumberConverter.KEY_COUNTRY_CODE, countryCode);
+    }
+
+    /**
+     * Returns instance of {@link NumberToCurrencyConverter}
+     * @return instance of NumberToCurrencyConverter
+     */
+    public NumberToPercentageConverter build(){
+        return new NumberToPercentageConverter(rawNumber, options);
+    }
 }
